de9139a4656066a6329a25237caf39f84dedbb70
yanyan69 <christiannarvaez300@gmail.com>
2025-09-16 19:58:09 +0800

    Add MySQL config logging

--- server.js
+++ server.js
@@ -289,3 +289,10 @@ app.use(express.static(path.join(__dirname, '.'))); // Serves index.html, css/, 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 }); // Serves index.html for all routes (SPA-like for guest sections)
+
+console.log('MySQL Config:', {
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE
+});
